b3265c3702c3b93bf0a727000179c2d849375c34
rikinovendra <90743431+rikinovendra@users.noreply.github.com>
2021-10-02 17:38:46 +0700

    pertemuan 3

---
+++ v_about.php
@@ -0,0 +1,24 @@
+<section>
+	<h1><?echo $judul ?></h1>
+	<h4>Nama</h4>
+	<ul type="disc">
+		<li>Nama Depan 	: Riki</li>
+		<li>Nama Belakang : Novendra</li>
+	</ul><br>
+
+	<h4>Alamat</h4>
+	<ul type="none">
+		<li>Jalan H Naba III No. 42</li>
+	</ul>
+
+	<h4>Tempat Lahir</h4>
+	<ul type="none">
+		Jakarta
+	</ul>
+
+	<h4>Olahraga Favorit</h4>
+	<ul>
+		<li>Bulutangkis</li>
+		<li>Catur</li>
+	</ul>
+</section>
